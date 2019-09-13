85d43229dd01158b8d26ba84e0b7ee8d3dddc1cf
Ivan Gil <igil.dev@gmail.com>
2019-09-12 20:12:16 -0400

    header component and shared module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ToolItemComponent } from './tools/tool-item/tool-item.component';
 import { ToolDetailComponent } from './tools/tool-detail/tool-detail.component';
 import { ToolEditComponent } from './tools/tool-edit/tool-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
